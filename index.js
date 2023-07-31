29c35d7da9bc978295119534d6543d8ff618bb77
skyyywalkr <rafay9641@gmail.com>
2023-08-01 03:07:07 +0500

    Webhook added

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require( 'express' );
+const app = express();
+app.use( express.json() );
+
+app.post( '/hook', ( req, res ) => {
+    console.log( 'received webhook', req.body );
+    res.sendStatus( 200 );
+} );
+
+app.listen( 9000, () => console.log( 'Node.js server started on port 9000.' ) );
