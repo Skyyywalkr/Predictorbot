32d43a31c5961983b40037544a2bd9b7169c3e2f
skyyywalkr <rafay9641@gmail.com>
2023-08-13 08:53:40 +0500

    completed and tested

--- index.js
+++ index.js
@@ -1,11 +1,159 @@
-const express = require( 'express' );
+const express = require('express');
+const fs = require('fs');
 const app = express();
-app.use( express.json() );
+const {Telegraf} = require('telegraf');
+const bot = new Telegraf('6474685886:AAGTqjT-05HyKpWBqWu0Jtapl5msFFDoC9E');
+// const bot = new Telegraf('6696209709:AAGaGqBbZ0zMGHekKM-JprKWpRJdnORpqhc');
 
-app.post( '/hook', ( req, res ) => {
-    console.log( 'received webhook', req.body );
-    console.log( 'received webhook 2', req );
-    res.sendStatus( 200 );
-} );
 
-app.listen( 80, () => console.log( 'Node.js server started on port 80.' ) );
+const data = JSON.parse(fs.readFileSync('DB.json', 'utf8'));
+
+
+// function searchMessageByCoinAndInterval(coinName, interval) {
+//   const regex = new RegExp(`${coinName}\\s*\\(([^)]*${interval}[^)]*)\\)`);
+  
+//   const foundMessage = data.messages.find((message) =>
+//     regex.test(message.message)
+//   );
+
+//   return foundMessage ? { messageID: foundMessage.messageID, chatID: foundMessage.chatID } : null;
+// }
+
+
+
+function searchMessageByCoinAndInterval(coinName, interval) {
+  const intervalPattern = interval.replace('-', '\\s*-?\\s*');
+  const regexWithInterval = new RegExp(`${coinName}\\s*\\(([^)]*${intervalPattern}[^)]*)\\)`, 'i');
+  const regexWithoutInterval = new RegExp(`${coinName}\\s*\\(`);
+
+  const foundMessageWithInterval = data.messages.find((message) => {
+    return regexWithInterval.test(message.message);
+  });
+
+  if (foundMessageWithInterval) {
+    return { messageID: foundMessageWithInterval.messageID, chatID: foundMessageWithInterval.chatID };
+  }
+
+  const foundMessageWithoutInterval = data.messages.find((message) => {
+    return regexWithoutInterval.test(message.message);
+  });
+
+  return foundMessageWithoutInterval ? { messageID: null, chatID: foundMessageWithoutInterval.chatID } : null;
+}
+
+
+
+app.use(express.json());
+
+
+
+
+app.post('/hook', (req, res) => {
+  
+  console.log('Received webhook:', req.body);
+  res.sendStatus(200);
+
+  const ticker = req.body.ticker;
+
+  const coin = ticker.substring(0, ticker.length - 4);
+  const interval = req.body.interval;
+  const close = req.body.close;
+  const action = req.body.action;
+
+  console.log('Coin:', coin);
+  console.log('Interval:', interval);
+
+  if (coin && close) {
+   
+    const isBuySignal = action.startsWith('BUY');
+
+
+    const messageArray = isBuySignal ? data.buy : data.sell;
+    const randomIndex = Math.floor(Math.random() * messageArray.length);
+    const randomMessage = messageArray[randomIndex];
+
+   
+    const formattedMessage = randomMessage.replace(/#Coin/g, coin +" "+interval).replace(/\$X/g, `$${close}`);
+
+    const messageData = searchMessageByCoinAndInterval(coin,interval);
+
+    console.log('Message Data:', messageData);
+
+    let messageID = messageData.messageID
+    let chatID = messageData.chatID
+
+    if(action === "BUY"){
+   
+    bot.telegram.sendMessage( chatID, formattedMessage, {
+    reply_to_message_id: messageID,
+  }).then((result) => {
+
+    const buyMessageID = result.message_id;
+
+    data.task.push({
+      'message': formattedMessage,
+      'buyMessageID': buyMessageID,
+      'chatID': chatID,
+      'coin':coin,
+      'interval':interval
+    })
+
+    fs.writeFileSync('db.json', JSON.stringify(data, null, 2));
+
+  });
+} else if(action === "SELL"){
+
+  const foundtask = data.task.find((message) =>
+  message.coin.includes(coin) &&  message.interval.includes(interval)
+)?data.task.find((message) =>
+message.coin.includes(coin) &&  message.interval.includes(interval)
+):null;
+
+const indexToRemove = data.task.findIndex((message) =>
+message.coin.includes(coin) &&  message.interval.includes(interval));
+
+if(foundtask){
+
+bot.telegram.sendMessage( foundtask.chatID, formattedMessage, {
+  reply_to_message_id: foundtask.buyMessageID,
+})
+}else{
+  console.log("No task Found")
+}
+if (indexToRemove !== -1) {
+  data.task.splice(indexToRemove, 1);
+
+  fs.writeFileSync('db.json', JSON.stringify(data, null, 2));
+
+  console.log(`Message removed successfully.`);
+} else {
+  console.log(`Messagen not found.`);
+}
+
+}
+  }
+});
+
+
+
+
+bot.on('channel_post', async (ctx) => {
+
+  const message = ctx.update.channel_post.text;
+  const messageID = ctx.update.channel_post.message_id;
+  const chatID  = ctx.update.channel_post.chat.id
+
+
+  data.messages.push({
+    'message': message,
+    'messageID': messageID,
+    'chatID': chatID
+  })
+
+  fs.writeFileSync('db.json', JSON.stringify(data, null, 2));
+
+})
+
+bot.launch();
+
+app.listen(80, () => console.log('Node.js server started on port 80.'));
