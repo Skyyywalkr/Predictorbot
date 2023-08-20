4b37df86359289d3b069b82e2801725a28ef3f58
skyyywalkr <rafay9641@gmail.com>
2023-08-20 06:53:27 +0500

    Change KEY

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const {Telegraf} = require('telegraf');
-// const bot = new Telegraf('6474685886:AAGTqjT-05HyKpWBqWu0Jtapl5msFFDoC9E');
-const bot = new Telegraf('6696209709:AAGaGqBbZ0zMGHekKM-JprKWpRJdnORpqhc');
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf('6474685886:AAGTqjT-05HyKpWBqWu0Jtapl5msFFDoC9E');
 
 
 const data = JSON.parse(fs.readFileSync('db.json', 'utf8'));
-
-
-// function searchMessageByCoinAndInterval(coinName, interval) {
-//   const regex = new RegExp(`${coinName}\\s*\\(([^)]*${interval}[^)]*)\\)`);
-  
-//   const foundMessage = data.messages.find((message) =>
-//     regex.test(message.message)
-//   );
-
-//   return foundMessage ? { messageID: foundMessage.messageID, chatID: foundMessage.chatID } : null;
-// }
-
 
 
 function searchMessageByCoinAndInterval(coinName, interval) {
@@ -42,14 +29,11 @@ function searchMessageByCoinAndInterval(coinName, interval) {
 }
 
 
-
 app.use(express.json());
 
 
-
-
 app.post('/hook', (req, res) => {
-  
+
   console.log('Received webhook:', req.body);
   res.sendStatus(200);
 
@@ -64,7 +48,7 @@ app.post('/hook', (req, res) => {
   console.log('Interval:', interval);
 
   if (coin && close) {
-   
+
     const isBuySignal = action.startsWith('BUY');
 
 
@@ -72,80 +56,78 @@ app.post('/hook', (req, res) => {
     const randomIndex = Math.floor(Math.random() * messageArray.length);
     const randomMessage = messageArray[randomIndex];
 
-   
-    const formattedMessage = randomMessage.replace(/#Coin/g, coin +" "+"(" +interval+")").replace(/\$X/g, `$${close}`);
 
-    const messageData = searchMessageByCoinAndInterval(coin,interval);
+    const formattedMessage = randomMessage.replace(/#Coin/g, coin + " " + "(" + interval + ")").replace(/\$X/g, `$${close}`);
+
+    const messageData = searchMessageByCoinAndInterval(coin, interval);
 
     console.log('Message Data:', messageData);
 
     let messageID = messageData.messageID
     let chatID = messageData.chatID
 
-    if(action === "BUY"){
-   
-    bot.telegram.sendMessage( chatID, formattedMessage, {
-    reply_to_message_id: messageID,
-  }).then((result) => {
+    if (action === "BUY") {
 
-    const buyMessageID = result.message_id;
+      bot.telegram.sendMessage(chatID, formattedMessage, {
+        reply_to_message_id: messageID,
+      }).then((result) => {
 
-    data.task.push({
-      'message': formattedMessage,
-      'buyMessageID': buyMessageID,
-      'chatID': chatID,
-      'coin':coin,
-      'interval':interval
-    })
+        const buyMessageID = result.message_id;
 
-    fs.writeFileSync('db.json', JSON.stringify(data, null, 2));
+        data.task.push({
+          'message': formattedMessage,
+          'buyMessageID': buyMessageID,
+          'chatID': chatID,
+          'coin': coin,
+          'interval': interval
+        })
 
-  });
-} else if(action === "SELL"){
+        fs.writeFileSync('db.json', JSON.stringify(data, null, 2));
 
-  const foundtask = data.task.find((message) =>
-  message.coin.includes(coin) &&  message.interval.includes(interval)
-)?data.task.find((message) =>
-message.coin.includes(coin) &&  message.interval.includes(interval)
-):null;
+      });
+    } else if (action === "SELL") {
 
-const indexToRemove = data.task.findIndex((message) =>
-message.coin.includes(coin) &&  message.interval.includes(interval));
+      const foundtask = data.task.find((message) =>
+        message.coin.includes(coin) && message.interval.includes(interval)
+      ) ? data.task.find((message) =>
+        message.coin.includes(coin) && message.interval.includes(interval)
+      ) : null;
 
-if(foundtask){
+      const indexToRemove = data.task.findIndex((message) =>
+        message.coin.includes(coin) && message.interval.includes(interval));
 
-bot.telegram.sendMessage( foundtask.chatID, formattedMessage, {
-  reply_to_message_id: foundtask.buyMessageID,
-})
+      if (foundtask) {
 
-if (indexToRemove !== -1) {
-  data.task.splice(indexToRemove, 1);
+        bot.telegram.sendMessage(foundtask.chatID, formattedMessage, {
+          reply_to_message_id: foundtask.buyMessageID,
+        })
 
-  fs.writeFileSync('db.json', JSON.stringify(data, null, 2));
+        if (indexToRemove !== -1) {
+          data.task.splice(indexToRemove, 1);
 
-  console.log(`Message removed successfully.`);
-} else {
-  console.log(`Messagen not found.`);
-}
+          fs.writeFileSync('db.json', JSON.stringify(data, null, 2));
 
-}else{
-  bot.telegram.sendMessage( chatID, formattedMessage, {
-    reply_to_message_id: messageID,
-  })
-}
+          console.log(`Message removed successfully.`);
+        } else {
+          console.log(`Messagen not found.`);
+        }
 
-}
+      } else {
+        bot.telegram.sendMessage(chatID, formattedMessage, {
+          reply_to_message_id: messageID,
+        })
+      }
+
+    }
   }
 });
-
-
 
 
 bot.on('channel_post', async (ctx) => {
 
   const message = ctx.update.channel_post.text;
   const messageID = ctx.update.channel_post.message_id;
-  const chatID  = ctx.update.channel_post.chat.id
+  const chatID = ctx.update.channel_post.chat.id
 
 
   data.messages.push({
